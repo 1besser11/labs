89fe9d857405df7706538a2927c093c9d13cb4bb
Slavick_Vinnitskyi <andrey.belyi1@gmail.com>
2017-03-16 21:30:55 +0200

    first commit

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+setlocale(LC_ALL,"RUSSIAN");
+cout<<"Проверка ввода и вывода строк\n";   
+
+for(int i=0;i<10;i++)
+printf("Hello world\n");
+
+return 0;
+} 
